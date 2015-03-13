da49fb87fde55503cf5665ecbed0157a3e4428b0
Nick Cardinal <nickcardinal95@gmail.com>
2015-03-13 12:03:48 -0700

    Homework 11

---
+++ Projects/README.md
@@ -0,0 +1,2 @@
+# CardinalNick_CSC17a_43950
+Spring CSC17a
---
+++ commit.bat
@@ -0,0 +1,5 @@
+git add --all
+git config --global user.email "nickcardinal95@gmail.com"
+git config --global user.name "Nick Cardinal"
+git commit -m "Homework 11"
+git push origin master
